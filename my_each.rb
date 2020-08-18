4ad510a04c6f3d63cccd2ad8d14e143f720bff79
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 23:47:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,13 @@ def my_each(argument)
   if block_given?
     i = 0 
     
-    while i < argument.length 
-      
-  
+    while i < argument.length
+      yield argument[i]
+      i += 1 
+    end 
+    
+    argument
+  else 
+    puts "?"
+  end
 end
